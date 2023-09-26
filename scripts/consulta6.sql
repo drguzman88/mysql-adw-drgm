9c9d5156f4d5c5e94f3602e706a0202d34e2194d
David Guzman <drguzman511@gmail.com>
2023-09-26 02:51:12 +0000

    carga

---
+++ scripts/consulta6.sql
@@ -0,0 +1,35 @@
+use adw;
+
+with datos as (
+
+select 
+    SalesOrderID,
+    OrderDate,
+    sales.CustomerID,
+    SalesPersonID,
+    sales.TerritoryID,
+    Subtotal,
+    concat(person.FirstName,' ',person.LastName) as CustomerName,
+    concat(emp.FirstName,' ',emp.LastName) as SalesName,
+    territory.Name as Territory,
+    employee.Gender as Gender
+from Sales_SalesOrderHeader as sales
+    join Sales_Customer as client 
+        on sales.CustomerID = client.CustomerID
+    left outer join Person_Person as person 
+        on client.PersonID = person.BusinessEntityID
+    join Sales_SalesPerson as sp
+        on sales.SalesPersonID = sp.BusinessEntityID
+    join Person_Person as emp
+        on sp.BusinessEntityID = emp.BusinessEntityID
+    join Sales_SalesTerritory as territory
+        on sales.TerritoryID=territory.TerritoryID
+    join HumanResources_Employee as employee 
+        on emp.BusinessEntityID=employee.BusinessEntityID
+)
+select 
+    Gender,
+    avg(subtotal) as PromedioVentas
+from datos
+group by Gender
+;
